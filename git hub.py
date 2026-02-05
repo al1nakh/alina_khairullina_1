392e7702682ba874d0706d2893c6c28b25622e0a
Alina Khairullina <alina2008khairullina@gmail.com>
2026-02-05 08:56:15 +0500

    fix analyze_text function

--- git hub.py
+++ git hub.py
@@ -1,10 +1,29 @@
+#1
+def analyze_text(text):
+    clean_text = ""
+    for i in text.lower():
+        if i.isalpha() or i == " ":
+            clean_text += i
+    vowels = "aeiouy"
+    unique_vowels = ""
+    for i in clean_text:
+        if i in vowels and i not in unique_vowels:
+            unique_vowels += i
+    vowel_count = len(unique_vowels)
+    words = clean_text.split()
+    result_words = ""
+    seen = ""
+    for i in words:
+        if len(i) >= 5:
+            if i[0] == i[-1]:
+                if i not in seen :
+                    result_words += i + " "
+                    seen += i + " "
+
+    return(vowel_count,result_words.strip())
+print(analyze_text("your level is high"))
+#-------------------------------------
+
 #2
-n=[5,8,6,11,7]
-result=[]
-for i in range(len(n)):
-    count_small=0
-    for j in range(i):
-        if n[j]<n[i]:
-            count_small+=1
-    result.append(count_small)
-print(result)
+
+
